5fc1190e1a6f49b7943ac92b27f15d5ae026a170
Carolina Margiotti <carolinamargiott2i@gmail.com>
2022-10-17 13:25:06 +0000

    arrumado lint

--- src/router/index.js
+++ src/router/index.js
@@ -2,25 +2,30 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView, //baixa todas as paginas de uma vez, faz demorar o primeiro acesso
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"), //baixa só a pagina e vai baixando toda vez que carrega
-  },
+	{
+		path: "/",
+		name: "home",
+		component: HomeView, //baixa todas as paginas de uma vez, faz demorar o primeiro acesso
+	},
+	{
+		path: "/about",
+		name: "about",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ "../views/AboutView.vue"), //baixa só a pagina e vai baixando toda vez que carrega
+	},
+	{
+		path: "/usuario",
+		name: "usuario",
+		component: () => import("../views/UsuarioView.vue"),
+	},
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+	history: createWebHashHistory(),
+	routes,
 });
 
 export default router;
